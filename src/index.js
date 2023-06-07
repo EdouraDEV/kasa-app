e4f25c72b4ac957a74c8df87e4d6b05b854c43b6
EdouraDEV <eddy.bouraba@outlook.fr>
2023-06-08 01:27:38 +0200

    Installation du node route Mise en place des première pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom';
+import './style/variables.scss';
+import HelloWorldAPropos from './pages/A-propos/APropos';
+import HelloWorldError from './pages/Error/Error';
+import HelloWorldFicheLogement from './pages/Fiche-logement/FicheLogement';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Router>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/APropos" element={<HelloWorldAPropos />} />
+        <Route path="/FicheLogement" element={<HelloWorldFicheLogement />} />
+        <Route path="*" element={<HelloWorldError />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
